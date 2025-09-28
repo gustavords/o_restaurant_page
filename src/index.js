587860c40931b0d9a25382ea6a6e7432fe52e7c4
gustavords <22622146+gustavords@users.noreply.github.com>
2025-09-28 13:20:18 -0400

    Create and add Webpack dependencies

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// index.js
+import "./styles.css";
+import { test } from "./test.js";
+import hammerSickle from "./hammer-sickle.svg";
+   
+const image = document.createElement("img");
+image.src = hammerSickle;
+   
+document.body.appendChild(image);
+console.log( test );
